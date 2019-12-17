ade237ebc8ff2f3027812ab79c7e10e7614e5296
Gabriel <gabriheron@hotmail.com>
2019-12-17 10:29:22 -0200

    [Config - Curso React] Adicionando heroku no CORS

--- src/main/java/br/com/gabriels/reactapi/infra/cors/CorsConfiguration.java
+++ src/main/java/br/com/gabriels/reactapi/infra/cors/CorsConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE",
-                "OPTIONS",	"HEAD",	"TRACE", "CONNECT");
+        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000", "http://react-client-gabriels.herokuapp.com/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",	"HEAD",	"TRACE", "CONNECT");
     }
 }
